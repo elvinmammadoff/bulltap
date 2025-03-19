1be60d2f4465f97d904fb443347952d91f87b65b
Elvin Mammadov <elvinmammadoff@gmail.com>
2025-03-19 14:55:51 +0400

    retina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1426,11 +1426,6 @@
       setCookie( gdprCookieName, 'shown', gdprExpireDays );
   });
 
- 
-  /****** Non retina image code ******/
-  $( "img:not([data-at2x])" ).each( function() {
-      $( this ).attr( 'data-no-retina', '' );
-  });
 
   /****** Window load ******/
   $( window ).on( 'load', function () {
